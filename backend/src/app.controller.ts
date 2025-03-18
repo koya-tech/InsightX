b88b15db41d1a9d156c00eaa47b801bc9e06c197
koya <koya9431ptr@gmail.com>
2025-03-18 17:32:02 +0000

    add simple logging feature

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CustomLoggerService } from './common/logger.service';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private readonly logger: CustomLoggerService,
+    ) {}
 
     @Get()
     getHello(): string {
+        this.logger.log('Hello API was called');
         return this.appService.getHello();
     }
 }
